2de5586e459d45a2d33009561b1ba33ffdafe92c
Jason Creviston <the@j4w8n.com>
2024-07-26 08:21:31 -0400

    refactor: move response handlers to new file

--- lib/exports/index.ts
+++ lib/exports/index.ts
@@ -1,23 +1,2 @@
 export { Xink } from '../index.ts'
-export const json = (data: any, init?: ResponseInit | undefined): Response => {
-  const body = JSON.stringify(data)
-  const headers = new Headers(init?.headers)
-
-  if (!headers.has('content-length'))
-    headers.set('content-length', new TextEncoder().encode(body).byteLength.toString())
-
-  if (!headers.has('content-type'))
-    headers.set('content-type', 'application/json')
-
-  return new Response(body, { ...init, headers })
-}
-
-export const text = (data: string, init?: ResponseInit | undefined): Response => {
-  const body = new TextEncoder().encode(data)
-  const headers = new Headers(init?.headers)
-
-  if (!headers.has('content-length'))
-    headers.set('content-length', body.byteLength.toString())
-  
-  return new Response(body, { ...init, headers })
-}
+export { json, text } from '../utils/helpers.ts'
