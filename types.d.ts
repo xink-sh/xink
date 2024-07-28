4442a3d67aa6590f79e3c20b90b33f7b8b3f7f2c
Jason Creviston <the@j4w8n.com>
2024-07-28 16:39:35 -0400

    chore: use top-level import for jsdoc types

--- types.d.ts
+++ types.d.ts
@@ -1,75 +1,10 @@
-import { MatchFunction } from "path-to-regexp";
-
-export type RequestEvent = {
-  req: Request;
-  headers: Omit<Headers, 'toJSON' | 'count' | 'getAll'>;
-  url: Omit<URL, 'createObjectURL' | 'revokeObjectURL' | 'canParse'>;
-  params: { [key: string]: any };
-  route: Route
-}
-export type MaybePromise<T> = T | Promise<T>
-export type Config = {
-  params?: string;
-  routes?: string;
-}
-export type DefaultConfig = {
-  params: string;
-  routes: string;
-}
-export type ValidatedConfig = {
-  params: string;
-  routes: string;
-}
-export type RouteType = 'static' | 'specific' | 'matcher' | 'dynamic' | 'low'
-export type SegmentType = {
-  name: 'static' | 'specific' | 'matcher' | 'dynamic' | 'low';
-  id: number;
-}
-export type Handler = (event: RequestEvent) => MaybePromise<Response> | null
-export type Handlers = {
-  [key: string]: Handler
-}
-type Params = { [key: string]: string }
-
-export type RouteInfo = {
-  params: Params;
-  handler: Handler | null;
-}
-export type Static = { [key: string]: any }
-export type Key = string | number;
-
-/**
- * xink Filesystem Router
- */
 export class Xink {
   async fetch(req: Request): Promise<Response>
 }
 
+type MaybePromise<T> = T | Promise<T>
 export type Handle = (input: { event: RequestEvent, resolve(event: RequestEvent): MaybePromise<Response> }) => MaybePromise<Response>
+
 export function json(data: any, init?: ResponseInit | undefined): Response
 export function text(data: string, init?: ResponseInit | undefined): Response
-export function sequence(...handlers: Handle[])
-
-/**
- * Medley types.
- */
-export type Params = { [key: string]: string }
-export type Route = { store: Store; params: Params; } | null;
-export type Store = { [key: string]: Handler }
-export type StoreFactory = () => Store
-export type Node = {
-  segment: string;
-  store: Store | null;
-  static_children: Map<number, Node> | null;
-  parametric_children: Map<string, ParametricNode> | null;
-  wildcard_store: Store | null;
-}
-export type Matcher = ((param: string) => boolean) | null;
-export type MatcherType = string | null;
-export type ParametricNode = {
-  matcher: Matcher;
-  matcher_type: MatcherType;
-  param_name: string;
-  store: Store | null;
-  static_child: Node | null;
-}
+export function sequence(...handlers: Handle[]): Handle
